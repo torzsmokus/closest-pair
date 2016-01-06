0071328c651921c3b314fcfd340f0fbe06b62dae
Eugene Ivantsov <eivantsov@codenvy.com>
2016-01-06 17:17:37 +0200

    added java console app

---
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.eclipse.che.examples;
+
+public class HelloWorld {
+    public static void main(String[] argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
+    }
+}
